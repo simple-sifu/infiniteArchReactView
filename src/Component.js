14fa883d8b54701993a380918d1ec315d2717fa1
Tommy Han <kattyntommy@gmail.com>
2023-01-17 00:16:57 +0000

    Update 2 files

--- src/Component.js
+++ src/Component.js
@@ -6,7 +6,18 @@ import ChildComponent from "./Child/ChildComponent";
 
 function App() {
   // binding output goes here
+  const [viewModel, setViewModel] = useState({});
 
+  useEffect(() => {
+    async function load() {
+      observe(presenter, "viewModel", obj => {
+        console.log("got here");
+        setViewModel(obj.newValue);
+      });
+      await presenter.load();
+    }
+    load();
+  }, []);
   return <>{/* binding input goes here */}</>;
 }
 
